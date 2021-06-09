fc69b43cbbed919d8e6034386209e0da2248fd3c
Quyền Vũ <vuquyen11a2k55@gmail.com>
2021-06-09 16:49:36 +0700

    Add Setting 2

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["vnq-blog-0118.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["vnq-blog-0118.herokuapp.com", "localhost"]
 
 
 # Application definition
