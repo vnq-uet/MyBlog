9aef640efb38e9b2aa89c1a05e1e6b6184dd9a0c
Quyền Vũ <vuquyen11a2k55@gmail.com>
2021-06-10 13:59:50 +0700

    changesetting

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = ['django-insecure-g98%mgb9iotk&2f!r%=ri810m)oefd8y3jb*1$$c_v-d6syjue']
+SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = ['django-insecure-g98%mgb9iotk&2f!r%=ri810m)oefd8y3jb*1$$c_v-d6syjue']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ["vnq-blog-0118.herokuapp.com", "localhost"]
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["vnq-blog-0118.herokuapp.com", "localhost"]
+# ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
