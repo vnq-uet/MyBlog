da20e8b040dd7d94e79dbbe3117dd67216c19f18
Quyền Vũ <vuquyen11a2k55@gmail.com>
2021-06-12 23:39:18 +0700

    Edit templates

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('contact/', view=views.get_contact, name='contact'),
     path('register/', view=views.register, name='register'),
     path('login/', view=auth_views.LoginView.as_view(template_name='pages/login.html'), name='login'),
-    path('logout/', view=auth_views.LogoutView.as_view(next_page='/'), name='logout')
+    path('logout/', view=auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('skill/', view=views.get_skill, name='show_list_skill'),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from .forms import RegisterForm
 from django.http import HttpResponseRedirect
+from blog.models import Post
 
 # Create your views here.
 def get_home(request):
-    return render(request, 'pages/home.html')
+    posts = Post.objects.all()
+    return render(request, 'pages/home.html', {'posts_size':len(posts)})
 
 def get_contact(request):
-    return render(request, 'pages/contact.html')
+    posts = Post.objects.all()
+    return render(request, 'pages/contact.html', {'posts_size':len(posts)})
+
+def get_skill(request):
+    posts = Post.objects.all()
+    return render(request, 'pages/skill.html', {'posts_size':len(posts)})
 
 def register(request):
     form = RegisterForm()
